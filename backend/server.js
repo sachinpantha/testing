48b54888e450f49f93581e21bb614b6d7ab8c19f
Sachiiiiin <sachinpantha69@gmail.com>
2025-10-14 17:14:56 +0545

    add

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://testing-black-delta.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
